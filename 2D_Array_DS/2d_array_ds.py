18df851c90e845cec779c283809f55078e4a9e45
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2020-07-24 22:37:38 +1000

    2d array ds

---
+++ 2D_Array_DS/2d_array_ds.py
@@ -0,0 +1,39 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hourglassSum function below.
+def hourglassSum(arr):
+    val = 0
+    rows = len(arr)
+    cols = len(arr[0])
+
+    r = 0
+    while r <= rows-3:
+        c = 0
+        while c <= cols-3:
+            _val = arr[r][c]+arr[r][c+1]+arr[r][c+2]+\
+                    arr[r+1][c+1]+\
+                    arr[r+2][c]+arr[r+2][c+1]+arr[r+2][c+2]  
+            if _val > val:
+                val = _val
+            c+=1
+        r+=1
+
+    return val
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
